ed7d38b444f6ad401d3e44d0984dde49a98d7d47
Luiz Claudio Pariz <lcpariz00@gmail.com>
2021-06-23 00:04:07 -0300

    Insert AddNumber.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,25 @@
   APAGAR ULTIMO ELEMENTO DIGITADO (  )
   SOMAR DOIS NÚMEROS (  ) */
 
-function AddNumber(num) {
-  
+var num = "";
+var num1 = [];
+var oldNum = [];
+var op = "";
+
+
+function AddNumber(number) {
+  console.log(number)
+  if(isNaN(number)) {
+    op = number;
+    oldNum = num1;
+    num = oldNum.join("")
+    num1.splice(length);
+    
+  } else if(num1.length < 8) {
+    num1.push(number); 
+    console.log(num1);
+
+  } else {
+    console.log("Erro: Máximo de 8 números.");
+  }
 }
