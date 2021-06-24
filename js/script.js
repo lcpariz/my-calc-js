f93f06a363ce612020e7d97281c455089e93de99
lcpariz <lcpariz00@gmail.com>
2021-06-24 17:43:29 -0300

    calc cases

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-/* PEGAR NÚMEROS E EXIBIR NO DISPLAY (  ) 
+/* PEGAR NÚMEROS ( x )
+  EXIBIR NO DISPLAY (  ) 
   LIMPAR DISPLAY (  )
   APAGAR ULTIMO ELEMENTO DIGITADO (  )
   SOMAR DOIS NÚMEROS (  ) */
@@ -7,21 +8,41 @@ var num = "";
 var num1 = [];
 var oldNum = [];
 var op = "";
+var limitNum = 8;
 
 
 function AddNumber(number) {
   console.log(number)
   if(isNaN(number)) {
-    op = number;
-    oldNum = num1;
-    num = oldNum.join("")
-    num1.splice(length);
+      op = number;
+      oldNum = num1;
+      num = oldNum.join("")
+      num1.splice(length);  
     
-  } else if(num1.length < 8) {
+  } else if(num1.length < limitNum) {
     num1.push(number); 
     console.log(num1);
+    if(oldNum != false) {
+      var num2 = num1.join("")
+      calc(num, num2, op);
+    }
 
   } else {
     console.log("Erro: Máximo de 8 números.");
+  }
+}
+
+function calc(n1, n2, op) {
+  n1 = parseFloat(n1);
+  n2 = parseFloat(n2);
+  
+  switch(op) {
+    case '+': break;
+    case '-': break;
+    case '/': break;
+    case '*': break;
+    default:
+      alert('Error#E-001: Algo errado aconteceu!');
+      document.location.reload();
   }
 }
